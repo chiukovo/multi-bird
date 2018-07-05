daa70e52945ed9021b60093812d4703e3122743e
chiukovo <q8156697@gmail.com>
2018-07-05 10:29:12 +0800

    fix init bug

---
+++ js/game.js
@@ -0,0 +1,7 @@
+/**
+ * Created 鳥遊戲 by chiuko
+ */
+var birdGame = {};
+birdGame.preload = function() {
+    console.log('preload success');
+};
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io').listen(server);
 
 app.use('/js',express.static(__dirname + '/js'));
+app.use('/css',express.static(__dirname + '/css'));
 app.get('/',function(req,res){
     res.sendFile(__dirname+'/index.html');
 });
